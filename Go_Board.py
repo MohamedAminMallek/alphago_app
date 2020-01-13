cfc0ba87115d5af6b173dd2e1156d96bfc194ac7
MohamedAminMallek <med.amin.mallek@gmail.com>
2020-01-13 17:09:57 +0100

    first commit

---
+++ Go_Board.py
@@ -0,0 +1,16 @@
+import goban.go as go
+class Go_Board(go.Board):
+
+    def __init__(self,board_size=9):
+        super(Go_Board,self).__init__(board_size)
+        # ....
+    
+    def get_legal_moves():
+        pass
+    
+    def game_over():
+        pass
+    
+    def push(point):
+        pass
+    
